ead54b47ec186f2fa8cdf3ae7e28f06b640e501e
hiplitewhat <hiplitewhat@gmail.com>
2025-06-03 17:12:01 +0800

    Update worker.ts

--- worker.ts
+++ worker.ts
@@ -23,7 +23,7 @@ async function getAptoideVersions(): Promise<{ aptoideVersion: string; aptoideVn
   };
 
   const aptoideVersion = await fetchVersion("https://roblox.en.aptoide.com/app");
-  const aptoideVngVersion = await fetchVersion("https://roblox-vng.en.aptoide.com/app");
+  const aptoideVngVersion = await fetchVersion("https://apkpure.com/roblox-vng/com.roblox.client.vnggames"); 
 
   return { aptoideVersion, aptoideVngVersion };
 }
